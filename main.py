c515ba7e8f3c2baa61781ab935698e824eb4b0e8
phzsantos <phzsantos2002@gmail.com>
2021-10-06 20:02:37 -0300

    Melhorando o algoritmo porem esta com bug no while

--- main.py
+++ main.py
@@ -5,10 +5,37 @@ estado = True
 while estado:
     input('Pense em um numero e depois aperte Enter ')
 
-    tentativa = random.randint(0,10)  # variavel sempre tem que ter nome minusculo e separado por _
+    tentativa = random.randint(0,10)
 
     resultado = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
 
-    if resultado == True:
+    if resultado:
         print('Adivinhei.')
-        estado = False 
+        estado = False
+    else:
+        resultado = int(input(f'Seu numero e maior ou menor que {tentativa}? (0 - Menor / 1 - Maior):' ))
+        
+        if resultado:
+            tentativa = random.randint(tentativa+1,10)
+
+            resultado = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
+
+            while not resultado:
+                tentativa = random.randint(tentativa+1,10)
+
+                resultado = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
+
+            print('Demorou mas eu advinhei rsrsrs')
+            estado = False    
+        else:
+            tentativa = random.randint(0,tentativa-1)
+
+            resultado = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
+
+            while not resultado:
+                tentativa = random.randint(0,tentativa-1)
+
+                resultado = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
+
+            print('Demorou mas eu advinhei rsrsrs')
+            estado = False

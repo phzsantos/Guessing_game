ceed272bbb3c887b6920740f2e8ef3061480a5c2
phzsantos <phzsantos2002@gmail.com>
2021-12-21 21:31:00 -0300

    Add more objectives on README; Code scalability improved; Feature add

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 
 resposta = 0
 tentativas_erradas = []
+maior = 10
+menor = 1
 
 while (resposta == 0):
     input('Pense em um numero de 1 a 10 e depois aperte Enter')
 
-    tentativa = random.randint(1,10)
+    tentativa = random.randint(menor,maior)
 
     resposta = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
     
@@ -20,21 +22,25 @@ while (resposta == 0):
             maior_ou_menor = int(input("Seu numero é maior ou menor? (0 - Menor/ 1 - Maior): "))
 
             if (maior_ou_menor == 0):
-                for i in range(tentativa,11):
+                for i in range(tentativa,maior+1):
                     if (i not in tentativas_erradas):
                         tentativas_erradas.append(i)
 
                 while (tentativa in tentativas_erradas):
-                    tentativa = random.randint(1,tentativa_anterior-1)
+                    tentativa = random.randint(menor,tentativa_anterior-1)
             else:
-                for i in range(tentativa,0,-1):
+                for i in range(tentativa,menor-1,-1):
                     if (i not in tentativas_erradas):
                         tentativas_erradas.append(i)
 
                 while (tentativa in tentativas_erradas):
-                    tentativa = random.randint(tentativa_anterior+1, 10)
-
-            resposta = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
+                    tentativa = random.randint(tentativa_anterior+1, maior)
+            
+            if (len(tentativas_erradas) == maior-1):
+                print(f'Então seu numero é {tentativa}!')
+                resposta = 1
+            else:
+                resposta = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
     
-            if (resposta == 1):
-                print("Acertei!")
+                if (resposta == 1):
+                    print("Acertei!")

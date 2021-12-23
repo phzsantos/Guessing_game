4b311149e550dafecd9af3d19673543c2ad5c0c2
phzsantos <phzsantos2002@gmail.com>
2021-12-22 22:28:05 -0300

    Added last features; Update README

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ It's a simple game, you choose a number between 1 and 10 and press ENTER, the al
 - [x] Guess the number
 - [x] Don't repeat tries
 - [x] Try the next number according to user answer
-- [ ] If the first try is 1 or 10 and it's not the user number automatically try another number lower or higher 
+- [x] If the first try is 1 or 10 and it's not the user number automatically try another number lower or higher 
 - [x] If there's only one number left, don't ask the user if that is the number choosed, just affirm that is
-- [ ] Don't let the user choose a invalid lower or higher option if the current try -1 or +1 is equal to last try
+- [x] Don't let the user choose a invalid lower or higher option if the current try -1 or +1 is equal to last try
 - [x] Make the code scalable
 - [ ] Translate the code to english
 
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while (resposta == 0):
     input('Pense em um numero de 1 a 10 e depois aperte Enter')
 
     tentativa = random.randint(menor,maior)
-
+    
     resposta = int(input(f'Seu numero e {tentativa}? (0 - Não / 1 - Sim): '))
     
     if (resposta == 1):
@@ -18,8 +18,13 @@ while (resposta == 0):
         while (resposta == 0):
             tentativas_erradas.append(tentativa)
             tentativa_anterior = tentativa
-                
-            maior_ou_menor = int(input("Seu numero é maior ou menor? (0 - Menor/ 1 - Maior): "))
+            
+            if ((tentativa != menor) and (tentativa != maior) and (tentativa-1 not in tentativas_erradas) and  (tentativa+1 not in tentativas_erradas)):
+                maior_ou_menor = int(input("Seu numero é maior ou menor? (0 - Menor/ 1 - Maior): ")) 
+            elif ((tentativa == menor) or (tentativa-1 in tentativas_erradas)):
+                maior_ou_menor = 1
+            else:
+                maior_ou_menor = 0
 
             if (maior_ou_menor == 0):
                 for i in range(tentativa,maior+1):
